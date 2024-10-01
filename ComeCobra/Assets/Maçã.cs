2a0d84ca03b5f26a7535d8ef53c4acd123e72341
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-01 19:21:03 -0300

    correção codigo da maca

--- ComeCobra/Assets/Maçã.cs
+++ ComeCobra/Assets/Maçã.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class maca : MonoBehaviour, IGeradorItem
+public class maca : MonoBehaviour, IGerar
 {
     // Gera uma posição aleatória no mapa.
-    public Vector2 GerarPosicao()
+    public Vector2 Posicionar(Vector2 posicao)
     {
-        int x = Random.Range(0, GerenciadorJogo.Instancia.larguraGrid);
-        int y = Random.Range(0, GerenciadorJogo.Instancia.alturaGrid);
+        int x = Random.Range(0, GameManager.Instancia.larguraGrid);
+        int y = Random.Range(0, GameManager.Instancia.alturaGrid);
         return new Vector2(x, y);
     }
 
