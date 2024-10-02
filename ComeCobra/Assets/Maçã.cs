6fd3e994a308c59e3dcd4f802c5d6a3791c70912
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-01 22:40:23 -0300

    definir local aleatório para a maça

--- ComeCobra/Assets/Maçã.cs
+++ ComeCobra/Assets/Maçã.cs
@@ -13,9 +13,26 @@ public class maca : MonoBehaviour
     {
         // Lista de todas as posições possíveis no grid
         List<Vector2> posicoesDisponiveis = new List<Vector2>();
+        for (int x = 0; x < GameManager.Instancia.larguraGrid; x++)
+        {
+            for (int y = 0; y < GameManager.Instancia.alturaGrid; y++)
+            {
+                Vector2 posicao = new Vector2(x, y);
 
-
+                // Se a posição não estiver ocupada pela cobra, adiciona à lista de disponíveis
+                if (!Cobra.Instancia.segmentosCobra.Contains(posicao))
+                {
+                    posicoesDisponiveis.Add(posicao);
+                }
+            }
+        }
+        // Escolhe uma posição aleatória da lista de disponíveis
+        if (posicoesDisponiveis.Count > 0)
+        {
+            novaPosicao = posicoesDisponiveis[Random.Range(0, posicoesDisponiveis.Count)];
+        }
     }
 
-
+}
+    
 }
