0071002b3dad3c6429f91503bb542971d57aab57
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-04 19:44:40 -0300

    criação do codigo de menu

--- ComeCobra/Assets/Menu.cs
+++ ComeCobra/Assets/Menu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputField campoLargura;  // Campo de texto para a largura do grid
+    public InputField campoAltura;   // Campo de texto para a altura do grid
+    public InputField campoVelocidade;  // Campo de texto para a velocidade da cobra
 
-    // Update is called once per frame
-    void Update()
+    // Aplica as configurações definidas pelo jogador no menu
+    public void AplicarConfiguracoes()
     {
-        
+        int largura = int.Parse(campoLargura.text);
+        int altura = int.Parse(campoAltura.text);
+        float velocidade = float.Parse(campoVelocidade.text);
+
+        // Define as configurações no Gerenciador do Jogo
+        GameManager.Instancia.DefinirTamanhoGrid(largura, altura);
+        GameManager.Instancia.DefinirVelocidadeCobra(velocidade);
     }
 }
