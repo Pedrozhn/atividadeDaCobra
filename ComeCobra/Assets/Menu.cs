03ebf01402a7dc69c81645afe22ffbfdf5e7a5b8
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-12 12:45:40 -0300

    método aplicar pra mudar o game manager

--- ComeCobra/Assets/Menu.cs
+++ ComeCobra/Assets/Menu.cs
@@ -7,4 +7,13 @@ public class Menu : MonoBehaviour
     [SerializeField] private TMP_InputField largura;  // Campo de texto para a largura do grid
     [SerializeField] private TMP_InputField altura;   // Campo de texto para a altura do grid
     [SerializeField] private TMP_InputField velocidade;  // Campo de texto para a velocidade da cobra
+
+    public void Aplicar()
+    {
+        int larg = int.Parse(largura.text);
+        int alt = int.Parse(altura.text);
+        float vel = float.Parse(velocidade.text);
+
+        GameManager.Instancia.DefinirTamanhoGrid(larg, alt);
+    }
 }
