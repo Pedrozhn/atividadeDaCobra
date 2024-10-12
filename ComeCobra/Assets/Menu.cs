1bfacc40085c80c52eb91e254b85784564a2f80b
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-12 12:43:16 -0300

    recomecei do 0

--- ComeCobra/Assets/Menu.cs
+++ ComeCobra/Assets/Menu.cs
@@ -1,33 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_InputField Largura;
-    [SerializeField]
-    private TMP_InputField Altura;
-    [SerializeField]
-    private TMP_InputField Velocidade;
-    void Start()
-    {
-        Largura = GameObject.Find("Largura").GetComponent<TMP_InputField>();
-        Altura = GameObject.Find("Altura").GetComponent<TMP_InputField>();
-        Velocidade = GameObject.Find("Velocidade da Cobra").GetComponent<TMP_InputField>();
-    }
-
-    // Aplica as configurações definidas pelo jogador no menu
-    public void AplicarConfiguracoes()
-    {
-        int largura = int.Parse(Largura.text);
-        int altura = int.Parse(Altura.text);
-        float velocidade = float.Parse(Velocidade.text);
-
-        // Define as configurações no Gerenciador do Jogo
-        GameManager.Instancia.DefinirTamanhoGrid(largura, altura);
-        GameManager.Instancia.DefinirVelocidadeCobra(velocidade);
-    }
+    [SerializeField] private TMP_InputField largura;  // Campo de texto para a largura do grid
+    [SerializeField] private TMP_InputField altura;   // Campo de texto para a altura do grid
+    [SerializeField] private TMP_InputField velocidade;  // Campo de texto para a velocidade da cobra
 }
