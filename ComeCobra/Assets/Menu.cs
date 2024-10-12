282c17007a9f6ebfb35aed759b04d5a7858f49b9
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-12 12:19:45 -0300

    troquei o InputField por TPM_Input
    para poder definir um texto e não um objeto

--- ComeCobra/Assets/Menu.cs
+++ ComeCobra/Assets/Menu.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public InputField Largura;  // Campo de texto para a largura do grid
-    public InputField Altura;   // Campo de texto para a altura do grid
-    public InputField Velocidade;  // Campo de texto para a velocidade da cobra
+    [SerializeField]
+    private TMP_InputField Largura;
+    [SerializeField]
+    private TMP_InputField Altura;
+    [SerializeField]
+    private TMP_InputField Velocidade;
+    void Start()
+    {
+        Largura = GameObject.Find("Largura").GetComponent<TMP_InputField>();
+        Altura = GameObject.Find("Altura").GetComponent<TMP_InputField>();
+        Velocidade = GameObject.Find("Velocidade da Cobra").GetComponent<TMP_InputField>();
+    }
 
     // Aplica as configurações definidas pelo jogador no menu
     public void AplicarConfiguracoes()
